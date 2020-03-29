0ded691353f9bb8af2ce023bea30853a22d395bb
Maxime Bartier <poulycroc.studio@gmail.com>
2020-03-29 20:59:40 +0200

    wip

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
 class ProductsController extends Controller
 {
     /**
@@ -14,21 +16,9 @@ class ProductsController extends Controller
         $data = [
             'shopName' => $id,
             'shopLogo' => 'https://www.stickpng.com/assets/images/58406746657b0e0e08612e45.png',
-            'product' =>
-                [
-                    'name'        => 'CaraPils Can. 33Cl',
-                    'price'       => '12,99€',
-                    'image1'       => 'https://www.mangebelge.com/10961-large_default/cara-pils-44-33cl.jpg',
-                    'image2'       => 'https://t2.ldh.be/96GsfY9oqZfqQ_ewvGNCaVAx1zc=/0x161:4000x2161/940x470/5b571b4d5532692548451081.jpg',
-                    'image3'       => 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Carapils.jpg',
-                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A delectus dignissimos dolores et facere illum, minus natus nesciunt omnis placeat praesentium quidem vel vitae. Harum iure molestiae officiis praesentium veritatis.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A delectus dignissimos dolores et facere illum, minus natus nesciunt omnis placeat praesentium quidem vel vitae. Harum iure molestiae officiis praesentium veritatis.<br><br>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A delectus dignissimos dolores et facere illum, minus natus nesciunt omnis placeat praesentium quidem vel vitae. Harum iure molestiae officiis praesentium veritatis.',
-                    'stock'       => true,
-                    'ref'         => $ref
-                ]
+            'product' => Product::all()
         ];
-            return view('pages/product', $data);
+        return view('pages/product', $data);
     }
 
     public function store(Request $request)
